0b2ddf3486530b8b45623b44981757fd527b491f
NapasIsSky <56857844+NapasIsSky@users.noreply.github.com>
2019-11-28 09:13:43 +0700

    ตัวอย่างจ้า

---
+++ src/component/Products.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import {Row, Col} from 'antd'
+import Product from './Product'
+
+export default class Products extends Component {
+    renderProducts(){
+        return this.props.products.map(product =>
+            <Col span={8} key={product.id}>
+                <Product productDetail={product} />
+            </Col>
+            )
+    }
+    render() {
+        return (
+            <Row>
+                {this.renderProducts()}
+            </Row>
+        )
+    }
+}
